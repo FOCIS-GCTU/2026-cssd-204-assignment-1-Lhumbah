0d20f4ad561c89cc410b0a6ec664c9dff327314d
Selorm Kamasa <kblumbah5967@gmail.com>
2026-05-30 18:07:07 +0000

    Add files via upload
    Submission of assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Selorm Kwabena Kamasa
+# Student ID: 2425403176
+# Email: kblumbah5967@gmail.com
+# Grader: Augustus
+#
+# On my honor, Selorm Kwabena Kamasa, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in small and large stylized block letters made up of the letters S, K, and K
 
+    print()
+    print("...SKK")
+    print()
+    print(".....SSSSSSSSSS........KKKK....KKKK.........KKKK....KKKK.....")
+    print("....SSSSSSSSSSS........KKK....KKK...........KKK....KKK.......")
+    print("...SSSS................KK....KK.............KK....KK.........")
+    print("...SSSS................KK...KK..............KK...KK..........")
+    print(".....SSSSSS............KKKKKK...............KKKKKK...........")
+    print("........SSSSS..........KK...KK..............KK...KK..........")
+    print("..........SSSSS........KK....KK.............KK....KK.........")
+    print("..........SSSSS........KKK....KKK...........KKK....KKK.......")
+    print("...SSSSSSSSSSS....**...KKK.....KKK.....**...KKK.....KKK....**")
+    print("...SSSSSSSSSS.....**...KKKK....KKKK...**....KKKK....KKKK...**")
+    print()
 
 
 main()
